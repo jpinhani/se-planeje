80854d43d278dd3b74e47232225634f82c78ee2a
jpinhani <jp.inhani@hotmail.com>
2020-02-08 15:48:23 -0300

    Atualização com Tela de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,16 +3,38 @@ import Title from '../components/Title'
 import Field from '../components/Field'
 import Button from '../components/Button'
 
+const styleLogin = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red'
+}
+
+const styleContainerButton = {
+    margin: '10px 0'
+}
 
 function Login(){
 
+
+    function handleSubmit(event) {
+       console.log(event.target)
+    }
+
     return (
-        <div>
-            <Title/>
-            <Field/>
-            <Field/>
-            <Button/>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div style={styleLogin}>
+                <Title/>
+                <div style={styleContainerButton}>
+                    <Field/>
+                </div>
+                <Field tipocampo='password' />
+                <div style={styleContainerButton}>
+                    <Button />
+                </div>
+            </div>
+        </form>
     )
 }
 
