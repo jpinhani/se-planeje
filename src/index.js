be0fb6e377d35caf7d78cec82ad067cb4814605d
jpinhani <jp.inhani@hotmail.com>
2020-02-16 17:23:20 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CadNatureza from './pages/cadNatureza/CadNatureza';
+// import CadNatureza from './pages/cadNatureza/CadNatureza';
+// import Header from './components/Header/Header'
 import './style/normalize.css'
 
 ReactDOM.render(<CadNatureza />, document.getElementById('root'));
---
+++ src/pages/Header/index.js
@@ -0,0 +1,3 @@
+import React from React;
+
+export default () => <h1>Header</h1>
