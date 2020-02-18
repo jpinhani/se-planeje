156a1117baef2c36204604ab1ff28cfe2c1ce276
jpinhani <jp.inhani@hotmail.com>
2020-02-17 23:20:14 -0300

    Update Menu and code InserNatureza

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -24,42 +24,57 @@ class SiderDemo extends React.Component {
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
+            {/* <Menu.Item key="1">
               <Icon type="pie-chart" />
               <span>Option 1</span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="desktop" />
               <span>Option 2</span>
-            </Menu.Item>
+            </Menu.Item> */}
             <SubMenu
               key="sub1"
               title={
                 <span>
-                  <Icon type="user" />
-                  <span>User</span>
+                  {/* <Icon type="cadastros" /> */}
+                  <span>Cadastros</span>
                 </span>
               }
             >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
+              <Menu.Item key="3">Categorias</Menu.Item>
+              <Menu.Item key="4">Contas</Menu.Item>
+              <Menu.Item key="5">Cartões</Menu.Item>
+              <Menu.Item key="6">Visões</Menu.Item>
             </SubMenu>
             <SubMenu
               key="sub2"
               title={
                 <span>
-                  <Icon type="team" />
-                  <span>Team</span>
+                  {/* <Icon type="team" /> */}
+                  <span>Previsto</span>
                 </span>
               }
             >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
+              <Menu.Item key="7">Planejar Despesas</Menu.Item>
+              <Menu.Item key="8">Planejar Receitas</Menu.Item>
             </SubMenu>
-            <Menu.Item key="9">
+            <SubMenu
+              key="sub3"
+              title={
+                <span>
+                  {/* <Icon type="team" /> */}
+                  <span>Real</span>
+                </span>
+              }
+            >
+              <Menu.Item key="9">Despesas</Menu.Item>
+              <Menu.Item key="10">Faturas</Menu.Item>
+              <Menu.Item key="11">Receitas</Menu.Item>
+              <Menu.Item key="12">Transferências</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="13">
               <Icon type="file" />
-              <span>File</span>
+              <span>Manual (Ajuda)</span>
             </Menu.Item>
           </Menu>
         </Sider>
@@ -67,8 +82,8 @@ class SiderDemo extends React.Component {
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              {/* <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>  */}
               <Nature />
             </Breadcrumb>
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
--- src/pages/Nature/index.js
+++ src/pages/Nature/index.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Input, Button } from 'antd'
 
 import './styles.scss'
 
 export default () => {
+
+  const [descricao, atualizaEstadoDescricao] = useState(null)
+  const [status, atualizaEstadoStatus] = useState(null)
+
+  function handleDescriptionChange(event) {
+    // console.log(event.target)
+    atualizaEstadoDescricao(event.target.value)
+  }
+
+  function handleStatusChange(event) {
+    // console.log(event.target)
+    atualizaEstadoStatus(event.target.value)
+  }
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -13,22 +26,25 @@ export default () => {
 
     const body = {
       idUser: 2,
-      descrNatureza: 'NuBank',
-      status: 'Ativo'
+      descrNatureza: descricao,
+      status: status
     }
 
+    console.log(body)
+
     axios.post(endpointAPI, body).then(function (result) {
-      console.log(result)
-    }).catch(function(err) {
+      // console.log(result)
+    }).catch(function (err) {
       console.log(err)
     })
-    
+
   }
 
   return (
     <div className='nature-page'>
       <form onSubmit={handleSubmit}>
-        <Input placeholder="Informe o nome da natureza" />
+        <Input name='natureDescription' onChange={handleDescriptionChange} placeholder="Informe o nome da natureza" />
+        <Input name='statusDescription' onChange={handleStatusChange} placeholder="Informe o Status" />
         <Button htmlType='submit'>Enviar</Button>
       </form>
     </div>
