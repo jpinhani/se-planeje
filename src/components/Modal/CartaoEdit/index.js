d1717320d5377e61578ae5a391f5e70260adf6ad
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-02-29 19:15:10 -0300

    Filizado update de cartao

--- src/components/Modal/CartaoEdit/index.js
+++ src/components/Modal/CartaoEdit/index.js
@@ -1,85 +1,108 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import axios from 'axios'
-import { Modal, Input } from 'antd'
+import { Icon, Modal, Input } from 'antd'
+import { listCards } from '../../../store/actions/generalCardAction.js'
 import 'antd/dist/antd.css';
 import './styles.scss'
-// import SelectCartao from '../../../pages/SelectCartao';
 
+class ModalCard extends React.Component {
+    constructor(props) {
+        super(props)
 
-export default () => {
+        this.state = {
+            visible: false,
+            cartao: this.props.data.CARTAO,
+            dtVencimento: this.props.data.DT_VENCIMENTO,
+            diacompra: this.props.data.DIA_COMPRA
+        }
+
+        this.showModal = this.showModal.bind(this)
+        this.handleCancel = this.handleCancel.bind(this)
+        this.handleCartao = this.handleCartao.bind(this)
+        this.handleDtVencimento = this.handleDtVencimento.bind(this)
+        this.handleDiaCompra = this.handleDiaCompra.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
     /* -------------------------------------  Comandos para Funcionamento do Modal*/
-    const [visible, setVisible] = useState(false)
+    showModal() {
+        this.setState({...this.state, visible: true })
+    };
 
-    // function showModal() {
-    //     setVisible(true)
-    // };
-
-
-    function handleCancel() {
-        setVisible(false);
-    }
+    handleCancel() {
+        this.setState({...this.state, visible: false })
+    };
     /* -------------------------------------  Comandos para Funcionamento do Modal*/
 
-    const [cartao, InsertCartao] = useState(null)
-    const [dtvencimento, InsertDtVencimento] = useState(null)
-    const [diacompra, InsertDiaCompra] = useState(null)
-
-    // const [status, InsertStatus] = useState(null)
-
-    function handleCartao(event) {
-        InsertCartao(event.target.value)
+    handleCartao(event) {
+        this.setState({...this.state, cartao: event.target.value })
     }
 
-    function handleDtVencimento(event) {
-        // console.log(event.target)
-        InsertDtVencimento(event.target.value)
+    handleDtVencimento(event) {
+        this.setState({...this.state, dtVencimento: event.target.value })
     }
 
-    function handleDiaCompra(event) {
-        // console.log(event.target)
-        InsertDiaCompra(event.target.value)
+    handleDiaCompra(event) {
+        this.setState({...this.state, diacompra: event.target.value })
     }
-    function handleSubmit(event) {
+
+    async handleSubmit(event) {
         event.preventDefault()
 
-        const endpointAPI = 'http://localhost:8082/api/cartoes'
+        const endpointAPI = `http://localhost:8082/api/cartoes/${this.props.data.ID}`
+
+        console.log(endpointAPI)
 
         const body = {
             idUser: 2,
-            cartao: cartao,
-            dtVencimento: dtvencimento,
-            diaCompra: diacompra,
+            cartao: this.state.cartao,
+            dtVencimento: this.state.dtVencimento,
+            diaCompra: this.state.diacompra,
             status: "Ativo"
         }
 
-        console.log(body)
+        await axios.put(endpointAPI, body)
+        const endpoint = `http://localhost:8082/api/cartoes/`
+        const result = await axios.get(endpoint)
+        const cards = result.data
+        console.log('cards', cards)
 
-        axios.post(endpointAPI, body).then(function (result) {
-            console.log(result)
-        }).catch(function (err) {
-            console.log(err)
-        })
-        alert("Cadastro Alterado com Sucesso")
-        setVisible(false)
+        this.props.listCards(cards)
+
+        this.setState({...this.state, visible: false })
     }
 
-
-    return <div>
-        <form onSubmit={handleSubmit}>
-            <Modal
-                title="Editar  Cartão de Crédito"
-                visible={visible}
-                onOk={handleSubmit}
-                onCancel={handleCancel}
-            >
-
-                <Input name='cartao' onChange={handleCartao} placeholder="Informe o nome do Cartão de Crédito" />
-                <Input type='number' onChange={handleDtVencimento} max='31' min='1' name='dtVencimento' placeholder="Informe o dia de Vencimento da Fatura " />
-                <Input type='number' onChange={handleDiaCompra} max='31' min='1' name='diaCompra' placeholder="Informe o melhor dia de Compra" />
-            </Modal>
-        </form >
-    </div >
-
-
+    render() {
+        return (
+            <div>
+                <Icon type="edit" style={{ fontSize: '18px', color: '#08c' }} title='Editar cartão' theme="twoTone" onClick={this.showModal} />
+                <form onSubmit={this.handleSubmit}>
+                    <Modal
+                        title="Editar Cartão de Crédito"
+                        visible={this.state.visible}
+                        onOk={this.handleSubmit}
+                        onCancel={this.handleCancel}
+                    >
+                        <Input name='cartao' value={this.state.cartao} onChange={this.handleCartao} placeholder="Informe o nome do Cartão de Crédito" />
+                        <Input type='number' value={this.state.dtVencimento} onChange={this.handleDtVencimento} max='31' min='1' name='dtVencimento' placeholder="Informe o dia de Vencimento da Fatura " />
+                        <Input type='number' value={this.state.diacompra} onChange={this.handleDiaCompra} max='31' min='1' name='diaCompra' placeholder="Informe o melhor dia de Compra" />
+                    </Modal>
+                </form>
+            </div >
+        )
+    }
 }
+
+const mapStateToProps = (state /*, ownProps*/) => {
+    return {
+        card: state.card
+    }
+}
+
+const mapDispatchToProps = { listCards }
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ModalCard)
--- src/pages/SelectCartao/index.js
+++ src/pages/SelectCartao/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import AddCartao from '../../components/Modal/Cartao/index'
-// import EditCartao from '../../components/Modal/CartaoEdit/index'
+import EditCartao from '../../components/Modal/CartaoEdit/index'
 import { listCards } from '../../store/actions/generalCardAction.js'
 import { Table, Divider, Icon, Input } from 'antd'
 import axios from 'axios'
@@ -9,48 +9,44 @@ import axios from 'axios'
 import 'antd/dist/antd.css';
 import './style.scss'
 
-const columns = [
-  {
-    title: 'Descrição do Cartão',
-    dataIndex: 'CARTAO',
-    key: 'CARTAO'
-  },
-  {
-    title: 'Dia de Vencimento',
-    dataIndex: 'DT_VENCIMENTO',
-    key: 'DT_VENCIMENTO'
-  },
-  {
-    title: 'Melhor dia de Compra',
-    dataIndex: 'DIA_COMPRA',
-    key: 'DIA_COMPRA'
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    // width: '40%',
-    render: () => (
-      <div className='ModeloBotoesGrid'>
-        < span className='ModeloBotoesGridDetalhes' >
-          <Icon type="edit" style={{ fontSize: '18px', color: '#08c' }}/>
-          {/* <Link to='/selectconta'><Icon type="edit" style={{ fontSize: '18px', color: '#08c' }} /></Link> */}
-        </span>
-        <Divider type="vertical" />
-        < span className='ModeloBotoesGridDetalhes' >
-          <Icon type="delete" style={{ fontSize: '18px', color: '#08c' }} />
-        </span >
-      </div>
-    ),
-  }
-]
-
 class SelectCartao extends React.Component {
+  columns() {
+    return [
+      {
+        title: 'Descrição do Cartão',
+        dataIndex: 'CARTAO',
+        key: 'CARTAO'
+      },
+      {
+        title: 'Dia de Vencimento',
+        dataIndex: 'DT_VENCIMENTO',
+        key: 'DT_VENCIMENTO'
+      },
+      {
+        title: 'Melhor dia de Compra',
+        dataIndex: 'DIA_COMPRA',
+        key: 'DIA_COMPRA'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+ 
+        render: card => (
+          <div className='ModeloBotoesGrid'>
+            <span className='ModeloBotoesGridDetalhes' >
+              <EditCartao data={card} /> 
+              <Icon type="delete" style={{ fontSize: '18px', color: '#08c' }} />
+            </span>
+          </div>
+        ),
+      }
+    ]
+  }
 
   async requestAPI() {
     const endpointAPI = 'http://localhost:8082/api/cartoes/'
     const result = await axios.get(endpointAPI)
     const cartao = result.data
-
     this.props.listCards(cartao)
   }
 
@@ -61,14 +57,12 @@ class SelectCartao extends React.Component {
   render() {
     return (
       <div>
-        {/* <EditCartao /> */}
         <div className='ViewCartao'>
           <AddCartao />
-          {/* <Input onChange={SearchCartao} name='cartao' placeholder="Procure aqui o cartão especifico" /> */}
           <Input name='cartao' placeholder="Procure aqui o cartão especifico" />
         </div>
         <Divider type="horizontal" />
-        <Table columns={columns} dataSource={this.props.card} />
+        <Table columns={this.columns()} dataSource={this.props.card} />
       </div >
     )
   }
