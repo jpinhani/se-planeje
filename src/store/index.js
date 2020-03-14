b1c737adced989af0e2e579a055c093cd6473fba
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-03-14 12:13:50 -0300

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,7 @@ import userReducer from './reducers/userReducer.js'
 import cardReducer from './reducers/generalCardReducer.js'
 import acountReducer from './reducers/generalAcountReducer.js'
 import categoryReducer from '../store/reducers/generalCategoryReducer'
-<<<<<<< HEAD
-
 import visionReducer from '../store/reducers/visionReducer'
-
-=======
-import visionReducer from '../store/reducers/visionReducer'
->>>>>>> 9bba2183e619c72a72c3614d00cf3c6533a5b0df
 import expenseReducer from '../store/reducers/generalExpenseReducer'
 
 const reducers = combineReducers({
@@ -19,11 +13,7 @@ const reducers = combineReducers({
   acount: acountReducer,
   category: categoryReducer,
   vision: visionReducer,
-  expense: expenseReducer,
-<<<<<<< HEAD
-=======
-
->>>>>>> 9bba2183e619c72a72c3614d00cf3c6533a5b0df
+  expense: expenseReducer
 })
 
 export default createStore(reducers)
