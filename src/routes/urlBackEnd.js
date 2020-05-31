3609e30452200efb51d70c5cda7f92e42e37150b
jpinhani <jp.inhani@hotmail.com>
2020-05-31 11:47:51 -0300

    Adjust Validation Vision

--- src/routes/urlBackEnd.js
+++ src/routes/urlBackEnd.js
@@ -1,9 +1,10 @@
 
-// const urlBackend = 'http://seplaneje-com.umbler.net/'
+const urlBackend = 'https://seplaneje-com.umbler.net/'
 // process.env.REACT_APP_API_URL
 // process.env.REACT_APP_API_URL
 // 'http://localhost:8082/' ||
-const urlBackend = 'http://seplaneje-com.umbler.net/'
+// const urlBackend = 'http://seplaneje-com.umbler.net:3000/'
+
 // const urlBackend = process.env.AMBIENTE
 
 // require('dotenv/config');
